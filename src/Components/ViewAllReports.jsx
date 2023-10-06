4ad9baa6816a337658d5ec80ce932a81abc7c984
Covenant Martins <martinscovenant1@gmail.com>
2023-10-06 22:29:47 +0100

    Thursday and friday commit

--- src/Components/ViewAllReports.jsx
+++ src/Components/ViewAllReports.jsx
@@ -1,314 +1,113 @@
-// // import React, { useState, useEffect } from 'react';
-// // import { Link } from 'react-router-dom';
-// // import axios from 'axios';
-// // import './View.css'
-
-// // export const ViewAllReports = ({ adminAccessToken }) => {
-// //   const [reportData, setReportData] = useState([]);
-// //   const [currentWeek, setCurrentWeek] = useState('');
-// //   const [loading, setLoading] = useState(true);
-
-// //   useEffect(() => {
-// //     const fetchData = async () => {
-// //       try {
-// //         const response = await axios.get(
-// //           'https://timesheet-api-main.onrender.com/view/reports/all',
-// //           {
-// //             params: {
-// //               'current-week': currentWeek,
-// //             },
-// //             headers: {
-// //               'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
-// //               Authorization: `Bearer ${adminAccessToken}`,
-// //             },
-// //           }
-// //         );
-
-// //         if (response.status === 200) {
-// //           setReportData(response.data.data);
-// //           setLoading(false);
-// //         } else {
-// //           console.error('Failed to fetch report data');
-// //           setLoading(false);
-// //         }
-// //       } catch (error) {
-// //         console.error('Error fetching report data:', error);
-// //         setLoading(false);
-// //       }
-// //     };
-
-// //     fetchData();
-// //   }, [currentWeek, adminAccessToken]);
-
-// //   return (
-// //     <div>
-// //       <h1>All Reports</h1>
-
-// //       <button onClick={() => setCurrentWeek('2023-10-1')}>View Week of 2023-10-1</button>
-// //       <br />
-// //       <Link to="/ViewSpecificReport">View Specific Reports</Link>
-
-// //       {loading ? (
-// //         <div>Loading...</div>
-// //       ) : (
-// //         <div className='row'>
-// //           <table>
-// //             <thead>
-// //               <tr className='inpute'>
-// //                 <th>User</th>
-// //                 <th>Day</th>
-// //                 <th>Date</th>
-// //                 <th>Duration</th>
-// //                 <th>Link</th>
-// //                 <th>Project</th>
-// //                 <th>Status</th>
-// //                 <th>Task</th>
-// //               </tr>
-// //             </thead>
-// //             <tbody>
-// //               {reportData.map((report, index) => (
-// //                 <tr key={index}>
-// //                   <td>{report.user.username}</td>
-// //                   {Object.keys(report.report).map((dayOfWeek) => (
-// //                     <React.Fragment key={dayOfWeek}>
-// //                       <td>{dayOfWeek}</td>
-// //                       {report.report[dayOfWeek] ? (
-// //                         <>
-// //                           <td>{report.report[dayOfWeek].date}</td>
-// //                           <td>{report.report[dayOfWeek].duration}</td>
-// //                           <td>{report.report[dayOfWeek].link}</td>
-// //                           <td>{report.report[dayOfWeek].project}</td>
-// //                           <td>{report.report[dayOfWeek].status}</td>
-// //                           <td>{report.report[dayOfWeek].task}</td>
-// //                         </>
-// //                       ) : (
-// //                         <td>No report for this day</td>
-// //                       )}
-// //                     </React.Fragment>
-// //                   ))}
-// //                 </tr>
-// //               ))}
-// //             </tbody>
-// //           </table>
-// //         </div>
-// //       )}
-// //     </div>
-// //   );
-// // };
-
-
-// import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-// import axios from 'axios';
-
-// export const ViewAllReports = ({ adminAccessToken }) => {
-//   const [reportData, setReportData] = useState([]);
-//   const [currentWeek, setCurrentWeek] = useState('');
-//   const [loading, setLoading] = useState(true);
-
-//   const handleWeekChange = (event) => {
-//     setCurrentWeek(event.target.value);
-//   };
-
-//   const fetchData = async () => {
-//     try {
-//       const response = await axios.get(
-//         'https://timesheet-api-main.onrender.com/view/reports/all',
-//         {
-//           params: {
-//             'current-week': currentWeek,
-//           },
-//           headers: {
-//             'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
-//             Authorization: `Bearer ${adminAccessToken}`,
-//           },
-//         }
-//       );
-
-//       if (response.status === 200) {
-//         setReportData(response.data.data);
-//         setLoading(false);
-//       } else {
-//         console.error('Failed to fetch report data');
-//         setLoading(false);
-//       }
-//     } catch (error) {
-//       console.error('Error fetching report data:', error);
-//       setLoading(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchData();
-//   }, [currentWeek, adminAccessToken]);
-
-//   return (
-//     <div>
-//       <h1>All Reports</h1>
-
-//       <div>
-//         <label htmlFor="weekInput">Enter Week:</label>
-//         <input
-//           type="text"
-//           id="weekInput"
-//           value={currentWeek}
-//           onChange={handleWeekChange}
-//           placeholder="YYYY-MM-DD"
-//         />
-//         <button onClick={fetchData}>View Reports</button>
-//       </div>
-//       <br />
-//       <Link to="/ViewSpecificReport">View Specific Reports</Link>
-
-//       {loading ? (
-//         <div>Loading...</div>
-//       ) : (
-//         <div>
-//           <table>
-//             <thead>
-//               <tr>
-//                 <th>User</th>
-//                 <th>Day</th>
-//                 <th>Date</th>
-//                 <th>Duration</th>
-//                 <th>Link</th>
-//                 <th>Project</th>
-//                 <th>Status</th>
-//                 <th>Task</th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//               {reportData.map((report, index) => (
-//                 <tr key={index}>
-//                   <td>{report.user.username}</td>
-//                   {Object.keys(report.report).map((dayOfWeek) => (
-//                     <React.Fragment key={dayOfWeek}>
-//                       <td>{dayOfWeek}</td>
-//                       {report.report[dayOfWeek] ? (
-//                         <>
-//                           <td>{report.report[dayOfWeek].date}</td>
-//                           <td>{report.report[dayOfWeek].duration}</td>
-//                           <td>{report.report[dayOfWeek].link}</td>
-//                           <td>{report.report[dayOfWeek].project}</td>
-//                           <td>{report.report[dayOfWeek].status}</td>
-//                           <td>{report.report[dayOfWeek].task}</td>
-//                         </>
-//                       ) : (
-//                         <td>No report for this day</td>
-//                       )}
-//                     </React.Fragment>
-//                   ))}
-//                 </tr>
-//               ))}
-//             </tbody>
-//           </table>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-export const ViewAllReports = ({ adminAccessToken }) => {
-  const [reportData, setReportData] = useState([]);
-  const [currentWeek, setCurrentWeek] = useState('');
-  const [loading, setLoading] = useState(true);
+export const ViewAllReports = () => {
+  const [reportData, setReportData] = useState({
+    date: '',
+    project: '',
+    task: '',
+    link: '',
+    status: '',
+    duration: '',
+  });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          'https://timesheet-api-main.onrender.com/view/reports/all',
-          {
-            params: {
-              'current-week': currentWeek,
-            },
-            headers: {
-              'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
-              Authorization: `Bearer ${adminAccessToken}`,
-            },
-          }
-        );
+  const [responseMessage, setResponseMessage] = useState('');
+  const [isReportRecorded, setIsReportRecorded] = useState(false);
 
-        if (response.status === 200) {
-          setReportData(response.data.data);
-          setLoading(false);
-        } else {
-          console.error('Failed to fetch report data');
-          setLoading(false);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setReportData({ ...reportData, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        'https://timesheet-api-main.onrender.com/record/report',
+        reportData,
+        {
+          headers: {
+            'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
+            Authorization: 'Bearer <user_access_token>', // Replace with the actual user access token
+          },
         }
-      } catch (error) {
-        console.error('Error fetching report data:', error);
-        setLoading(false);
-      }
-    };
+      );
 
-    fetchData();
-  }, [currentWeek, adminAccessToken]);
+      setResponseMessage(response.data.message);
+      setIsReportRecorded(true);
+    } catch (error) {
+      console.error('Error recording report:', error);
+      setResponseMessage('Error recording report.');
+    }
+  };
 
   return (
     <div>
-      <h1>All Reports</h1>
-
-      <button onClick={() => setCurrentWeek('2023-10-1')}>View Week of 2023-10-1</button>
-      <br />
-      <Link to="/ViewSpecificReport">View Specific Reports</Link>
-
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
+      <h1>Record Report</h1>
+      <form onSubmit={handleSubmit}>
         <div>
-          {reportData.map((report, index) => (
-            <div key={index}>
-              <h2>User: {report.user.username}</h2>
-              {Object.keys(report.report).map((dayOfWeek) => (
-                <div key={dayOfWeek}>
-                  <label>{dayOfWeek}:</label>
-                  {report.report[dayOfWeek] ? (
-                    <>
-                      <input
-                        type="text"
-                        value={report.report[dayOfWeek].date}
-                        readOnly
-                      />
-                      <input
-                        type="text"
-                        value={report.report[dayOfWeek].duration}
-                        readOnly
-                      />
-                      <input
-                        type="text"
-                        value={report.report[dayOfWeek].link}
-                        readOnly
-                      />
-                      <input
-                        type="text"
-                        value={report.report[dayOfWeek].project}
-                        readOnly
-                      />
-                      <input
-                        type="text"
-                        value={report.report[dayOfWeek].status}
-                        readOnly
-                      />
-                      <input
-                        type="text"
-                        value={report.report[dayOfWeek].task}
-                        readOnly
-                      />
-                    </>
-                  ) : (
-                    <div>No report for this day</div>
-                  )}
-                </div>
-              ))}
-            </div>
-          ))}
+          <label>Date:</label>
+          <input
+            type="date"
+            name="date"
+            value={reportData.date}
+            onChange={handleChange}
+            required
+          />
         </div>
-      )}
+        <div>
+          <label>Project:</label>
+          <input
+            type="text"
+            name="project"
+            value={reportData.project}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Task:</label>
+          <input
+            type="text"
+            name="task"
+            value={reportData.task}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Link:</label>
+          <input
+            type="url"
+            name="link"
+            value={reportData.link}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Status:</label>
+          <input
+            type="text"
+            name="status"
+            value={reportData.status}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Duration:</label>
+          <input
+            type="text"
+            name="duration"
+            value={reportData.duration}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Submit Report</button>
+      </form>
+      {isReportRecorded && <p>{responseMessage}</p>}
     </div>
   );
 };
--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,59 +1,89 @@
-
-// Login.js
 import React, { useState } from 'react';
 import axios from 'axios';
-import './Login.css'
 
 export const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [formData, setFormData] = useState({
+    username: 'admin.root',
+    password: 'admin.root',
+  });
 
-  const handleLogin = async () => {
+  const [response, setResponse] = useState(null);
+  const [isLoading, setIsLoading] = useState(false); // Add loading state
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setIsLoading(true); // Set loading to true when making the request
+
     try {
       const response = await axios.post(
         'https://timesheet-api-main.onrender.com/admin/login',
         {
-         'username': "admin.root",
-         'password': "admin.root"
+          username: formData.username,
+          password: formData.password,
         },
         {
           headers: {
             'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
-            'Content-Type': 'application/json',
           },
         }
       );
 
-      if (response.data.status) {
-
-        console.log('Login successful');
-      } else {
-  
-        console.error('Login failed');
-      }
+      setResponse(response.data);
     } catch (error) {
-      console.error('Error during login:', error);
+      console.error('Login failed:', error);
+      setResponse({ message: 'Login failed', status: false });
+    } finally {
+      setIsLoading(false); // Set loading back to false regardless of success or failure
     }
   };
 
   return (
     <div>
       <h1>Admin Login</h1>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? 'Logging in...' : 'Login'}
+        </button>
+      </form>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        response && (
+          <div>
+            <p>Status: {response.status ? 'Success' : 'Failed'}</p>
+            <p>Message: {response.message}</p>
+            {response.access_token && (
+              <p>Access Token: {response.access_token}</p>
+            )}
+          </div>
+        )
+      )}
     </div>
   );
 };
-
 
