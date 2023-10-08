312e4450ae4955845e9efc954a0d189b81296bb4
Covenant Martins <martinscovenant1@gmail.com>
2023-10-09 00:02:01 +0100

    sundays commit

--- src/Components/ViewAllReports.jsx
+++ src/Components/ViewAllReports.jsx
@@ -1,3 +1,125 @@
+// import React, { useState } from 'react';
+// import axios from 'axios';
+
+// export const ViewAllReports = () => {
+//   const [reportData, setReportData] = useState({
+//     date: '',
+//     project: '',
+//     task: '',
+//     link: '',
+//     status: '',
+//     duration: '',
+//   });
+
+//   const [responseMessage, setResponseMessage] = useState('');
+//   const [isReportRecorded, setIsReportRecorded] = useState(false);
+//   const navigateToAnotherPage = () => {
+//   window.location.href = 'another-page-url';
+
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+//     setReportData({ ...reportData, [name]: value });
+//   };
+
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     try {
+//       const response = await axios.post(
+//         'https://timesheet-api-main.onrender.com/record/report',
+//         reportData,
+//         {
+//           headers: {
+//             'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
+//             Authorization: 'Bearer <user_access_token>', // Replace with the actual user access token
+//           },
+//         }
+//       );
+
+//       setResponseMessage(response.data.message);
+//       setIsReportRecorded(true);
+//     } catch (error) {
+//       console.error('Error recording report:', error);
+//       setResponseMessage('Error recording report.');
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h1>Record Report</h1>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label>Date:</label>
+//           <input
+//             type="date"
+//             name="date"
+//             value={reportData.date}
+//             onChange={handleChange}
+//             required
+//           />
+//         </div>
+//         <div>
+//           <label>Project:</label>
+//           <input
+//             type="text"
+//             name="project"
+//             value={reportData.project}
+//             onChange={handleChange}
+//             required
+//           />
+//         </div>
+//         <div>
+//           <label>Task:</label>
+//           <input
+//             type="text"
+//             name="task"
+//             value={reportData.task}
+//             onChange={handleChange}
+//             required
+//           />
+//         </div>
+//         <div>
+//           <label>Link:</label>
+//           <input
+//             type="url"
+//             name="link"
+//             value={reportData.link}
+//             onChange={handleChange}
+//             required
+//           />
+//            {reportData.link && (
+//     <a href={reportData.link} target="_blank" rel="noopener noreferrer">
+//       Open Link
+//       </a>
+//        )}
+//         </div>
+//         <div>
+//           <label>Status:</label>
+//           <input
+//             type="text"
+//             name="status"
+//             value={reportData.status}
+//             onChange={handleChange}
+//             required
+//           />
+//         </div>
+//         <div>
+//           <label>Duration:</label>
+//           <input
+//             type="text"
+//             name="duration"
+//             value={reportData.duration}
+//             onChange={handleChange}
+//             required
+//           />
+//         </div>
+//         {/* <button type="submit">Submit Report</button>*/} 
+//         <button onClick={navigateToAnotherPage}>Go to Another Page</button>
+//       </form>
+//       {isReportRecorded && <p>{responseMessage}</p>}
+//     </div>
+//   );
+// };
+
 import React, { useState } from 'react';
 import axios from 'axios';
 
@@ -13,6 +135,11 @@ export const ViewAllReports = () => {
 
   const [responseMessage, setResponseMessage] = useState('');
   const [isReportRecorded, setIsReportRecorded] = useState(false);
+
+  // Define the navigateToAnotherPage function
+  const navigateToAnotherPage = () => {
+    window.location.href = 'ViewAllReports';
+  };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -84,6 +211,11 @@ export const ViewAllReports = () => {
             onChange={handleChange}
             required
           />
+          {reportData.link && (
+            <a href={reportData.link} target="_blank" rel="noopener noreferrer">
+              Open Link
+            </a>
+          )}
         </div>
         <div>
           <label>Status:</label>
@@ -105,11 +237,10 @@ export const ViewAllReports = () => {
             required
           />
         </div>
-        <button type="submit">Submit Report</button>
+        {/* <button type="submit">Submit Report</button> */}
+        <button onClick={navigateToAnotherPage}>Go to Another Page</button>
       </form>
       {isReportRecorded && <p>{responseMessage}</p>}
     </div>
   );
 };
-
-
--- src/Components/ViewSpecificReport.jsx
+++ src/Components/ViewSpecificReport.jsx
@@ -3,20 +3,40 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export const ViewSpecificReport = ({ userId }) => {
-  const [reportData, setReportData] = useState([]);
+  const [reportData, setReportData] = useState({
+    date: '',
+    project: '',
+    task: '',
+    status: '',
+    link: '',
+    duration: '',
+  });
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchSpecificReport = async () => {
       try {
-        const response = await axios.get(`https://timesheet-api-main.onrender.com/view/reports/${userId}`, {
-          headers: {
-            'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
-            'Authorization': 'Bearer <admin_access_token>',
-          },
+        
+        const response = await axios.get(
+          `https://timesheet-api-main.onrender.com/view/reports/${userId}`,
+          {
+            headers: {
+              'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
+              Authorization: 'Bearer <admin_access_token>',
+            },
+          }
+        );
+
+        const report = response.data.data.report;
+        setReportData({
+          date: report.date,
+          project: report.project,
+          task: report.task,
+          status: report.status,
+          link: report.link,
+          duration: report.duration,
         });
 
-        setReportData(response.data.data.report);
         setLoading(false);
       } catch (error) {
         console.error('Error fetching specific report:', error);
@@ -27,39 +47,86 @@ export const ViewSpecificReport = ({ userId }) => {
     fetchSpecificReport();
   }, [userId]);
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setReportData({ ...reportData, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // Add your submit logic here, e.g., send the updated data to the server
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
+    <div className="report-container">
       <h1>Report</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Day</th>
-            <th>Date</th>
-            <th>Project</th>
-            <th>Task</th>
-            <th>Status</th>
-            <th>Link</th>
-            <th>Duration</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.entries(reportData).map(([day, report], index) => (
-            <tr key={index}>
-              <td>{day}</td>
-              <td>{report.date}</td>
-              <td>{report.project}</td>
-              <td>{report.task}</td>
-              <td>{report.status}</td>
-              <td>{report.link}</td>
-              <td>{report.duration}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <form onSubmit={handleSubmit}>
+        <div className="input">
+          <label>Date:</label>
+          <input
+            type="date"
+            name="date"
+            value={reportData.date}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div  className="input">
+          <label>Project:</label>
+          <input
+            type="text"
+            name="project"
+            value={reportData.project}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="input">
+          <label>Task:</label>
+          <input
+            type="text"
+            name="task"
+            value={reportData.task}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="input">
+          <label>Status:</label>
+          <input
+            type="text"
+            name="status"
+            value={reportData.status}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="input">
+          <label>Link:</label>
+          <input
+            type="url"
+            name="link"
+            value={reportData.link}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="input">
+          <label>Duration:</label>
+          <input
+            type="text"
+            name="duration"
+            value={reportData.duration}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Submit Report</button>
+      </form>
     </div>
   );
 };
