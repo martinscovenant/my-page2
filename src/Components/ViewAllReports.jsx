5b54de1ba652cc706025b82ed6f48beecabf4a4a
Covenant Martins <martinscovenant1@gmail.com>
2023-11-02 23:51:51 +0100

    Thursday commit

--- src/Components/ViewAllReports.jsx
+++ src/Components/ViewAllReports.jsx
@@ -1,246 +1,81 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-
-// export const ViewAllReports = () => {
-//   const [reportData, setReportData] = useState({
-//     date: '',
-//     project: '',
-//     task: '',
-//     link: '',
-//     status: '',
-//     duration: '',
-//   });
-
-//   const [responseMessage, setResponseMessage] = useState('');
-//   const [isReportRecorded, setIsReportRecorded] = useState(false);
-//   const navigateToAnotherPage = () => {
-//   window.location.href = 'another-page-url';
-
-//   const handleChange = (event) => {
-//     const { name, value } = event.target;
-//     setReportData({ ...reportData, [name]: value });
-//   };
-
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-//     try {
-//       const response = await axios.post(
-//         'https://timesheet-api-main.onrender.com/record/report',
-//         reportData,
-//         {
-//           headers: {
-//             'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
-//             Authorization: 'Bearer <user_access_token>', // Replace with the actual user access token
-//           },
-//         }
-//       );
-
-//       setResponseMessage(response.data.message);
-//       setIsReportRecorded(true);
-//     } catch (error) {
-//       console.error('Error recording report:', error);
-//       setResponseMessage('Error recording report.');
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h1>Record Report</h1>
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           <label>Date:</label>
-//           <input
-//             type="date"
-//             name="date"
-//             value={reportData.date}
-//             onChange={handleChange}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <label>Project:</label>
-//           <input
-//             type="text"
-//             name="project"
-//             value={reportData.project}
-//             onChange={handleChange}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <label>Task:</label>
-//           <input
-//             type="text"
-//             name="task"
-//             value={reportData.task}
-//             onChange={handleChange}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <label>Link:</label>
-//           <input
-//             type="url"
-//             name="link"
-//             value={reportData.link}
-//             onChange={handleChange}
-//             required
-//           />
-//            {reportData.link && (
-//     <a href={reportData.link} target="_blank" rel="noopener noreferrer">
-//       Open Link
-//       </a>
-//        )}
-//         </div>
-//         <div>
-//           <label>Status:</label>
-//           <input
-//             type="text"
-//             name="status"
-//             value={reportData.status}
-//             onChange={handleChange}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <label>Duration:</label>
-//           <input
-//             type="text"
-//             name="duration"
-//             value={reportData.duration}
-//             onChange={handleChange}
-//             required
-//           />
-//         </div>
-//         {/* <button type="submit">Submit Report</button>*/} 
-//         <button onClick={navigateToAnotherPage}>Go to Another Page</button>
-//       </form>
-//       {isReportRecorded && <p>{responseMessage}</p>}
-//     </div>
-//   );
-// };
-
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Helmet } from 'react-helmet';
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const ViewAllReports = () => {
-  const [reportData, setReportData] = useState({
-    date: '',
-    project: '',
-    task: '',
-    link: '',
-    status: '',
-    duration: '',
-  });
+    const [report, setReport] = useState([]);
+    const currentDate = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000);
+    const formattedDate = currentDate.toISOString().split('T')[0];
+    
+    useEffect(() => {
+        const accessToken = sessionStorage.getItem('access_token');
+        const apiUrl = `https://timesheet-api-main.onrender.com/view/reports/all?current-week=${formattedDate}`;
+        
+        fetch(apiUrl, {
+            headers: {
+                'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
+                'Authorization': `Bearer ${accessToken}`
+            }
+        })
+        .then(response => response.json())
+        .then(data => data.status && setReport(data.data))
+        .catch(error => console.error("Error fetching data:", error));
+    }, [formattedDate]);
 
-  const [responseMessage, setResponseMessage] = useState('');
-  const [isReportRecorded, setIsReportRecorded] = useState(false);
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        sessionStorage.removeItem('access_token');
+        navigate('/');
+    };
 
-  // Define the navigateToAnotherPage function
-  const navigateToAnotherPage = () => {
-    window.location.href = 'ViewAllReports';
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setReportData({ ...reportData, [name]: value });
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post(
-        'https://timesheet-api-main.onrender.com/record/report',
-        reportData,
-        {
-          headers: {
-            'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
-            Authorization: 'Bearer <user_access_token>', // Replace with the actual user access token
-          },
-        }
-      );
-
-      setResponseMessage(response.data.message);
-      setIsReportRecorded(true);
-    } catch (error) {
-      console.error('Error recording report:', error);
-      setResponseMessage('Error recording report.');
-    }
-  };
-
-  return (
-    <div>
-      <h1>Record Report</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Date:</label>
-          <input
-            type="date"
-            name="date"
-            value={reportData.date}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Project:</label>
-          <input
-            type="text"
-            name="project"
-            value={reportData.project}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Task:</label>
-          <input
-            type="text"
-            name="task"
-            value={reportData.task}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Link:</label>
-          <input
-            type="url"
-            name="link"
-            value={reportData.link}
-            onChange={handleChange}
-            required
-          />
-          {reportData.link && (
-            <a href={reportData.link} target="_blank" rel="noopener noreferrer">
-              Open Link
-            </a>
-          )}
-        </div>
-        <div>
-          <label>Status:</label>
-          <input
-            type="text"
-            name="status"
-            value={reportData.status}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Duration:</label>
-          <input
-            type="text"
-            name="duration"
-            value={reportData.duration}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        {/* <button type="submit">Submit Report</button> */}
-        <button onClick={navigateToAnotherPage}>Go to Another Page</button>
-      </form>
-      {isReportRecorded && <p>{responseMessage}</p>}
-    </div>
-  );
+    return (
+        <>
+            <Helmet>
+                <title>VIEW ALL REPORTS</title>
+            </Helmet>
+            <div className="w-full p-1 h-11 flex justify-end my-4">
+                <button className="bg-blue-500 text-white rounded-md p-1 mr-[3%]" onClick={handleLogout}>LOGOUT</button>
+            </div>
+            <div className="sm:overflow-x-scroll lg:overflow-x-hidden">
+                <table className="mx-auto my-[10%] w-4/5">
+                    <thead>
+                        <tr className="border-2 border-solid border-black">
+                            <th>Date</th>
+                            <th>Day</th>
+                            <th>Project</th>
+                            <th>Task</th>
+                            <th>Status</th>
+                            <th>Duration</th>
+                            <th>Link</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {report.map((item, index) => (
+                            <tr key={index} className="border-2 border-solid border-black">
+                                <td>{item.report.date}</td>
+                                <td>{item.report["day-of-week"]}</td>
+                                <td>{item.report.project}</td>
+                                <td>{item.report.task}</td>
+                                <td>{item.report.status}</td>
+                                <td>{item.report.duration}</td>
+                                <td className="text-blue-500">
+                                    <a
+                                        href={item.report.link.startsWith("http") ? item.report.link : `http://${item.report.link}`}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        {item.report.link}
+                                    </a>
+                                </td>
+                                <Link to={`/view/${item.user.id}`}>
+                                    <button className="bg-blue-500 p-2 rounded-lg ml-[5%]">View</button>
+                                </Link>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
 };
