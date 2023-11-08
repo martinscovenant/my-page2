565eeaacebc0fc822aee1d3978f7a8ed557b7592
Covenant Martins <martinscovenant1@gmail.com>
2023-11-08 23:21:41 +0100

    Wenesday commit

--- src/Components/ViewAllReports.jsx
+++ src/Components/ViewAllReports.jsx
@@ -9,33 +9,31 @@ export const ViewAllReports = () => {
     currentDate.setDate(currentDate.getDate() - 7);
     const formattedDate = currentDate.toISOString().split('T')[0];
 
-    useEffect(() => {
+      useEffect(() => {
         const accessToken = sessionStorage.getItem('access_token');
-        try {
-            const apiUrl = `https://timesheet-api-main.onrender.com/view/reports/all?current-week=${formattedDate}`;
-            fetch(apiUrl, {
-                method: 'GET',
-                headers: {
-                    'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            })
-                .then(response => {
-                    if (response.status === 200) {
-                        return response.json();
-                    } else {
-                        console.log('Response:', response);
-                        throw new Error(`Failed to fetch data. Status: ${response.status}`);
-                    }
-                })
-                .then(data => {
-                    if (data.status) {
-                        setReport(data.data);
-                    }
-                });
-        } catch (error) {
+        const apiUrl = `https://timesheet-api-main.onrender.com/view/reports/all?current-week=${formattedDate}`;
+
+        fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
+                'Authorization': `Bearer ${accessToken}`
+            }
+        })
+        .then(response => {
+            if (response.ok) {  // Check if the response status is in the 2xx range
+                return response.json();
+            } else {
+                console.log('Response:', response);
+                throw new Error(`Failed to fetch data. Status: ${response.status}`);
+            }
+        })
+        .then(data => {
+            setReport(data.data);
+        })
+        .catch(error => {
             console.error("Error fetching data:", error);
-        }
+        });
     }, [formattedDate]);
 
     const navigate = useNavigate();
@@ -44,7 +42,6 @@ export const ViewAllReports = () => {
         sessionStorage.removeItem('access_token');
         navigate('/');
     };
-
     return (
         <>
             <Helmet>
