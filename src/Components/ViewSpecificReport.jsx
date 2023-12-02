a9d9895f9eda7ce57a9abead109cd1eb53222665
Covenant Martins <martinscovenant1@gmail.com>
2023-12-02 01:21:53 +0100

    Friday commit

--- src/Components/ViewSpecificReport.jsx
+++ src/Components/ViewSpecificReport.jsx
@@ -1,23 +1,34 @@
-import React, { useState, useEffect } from "react";
-import { Helmet } from "react-helmet";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useNavigate, useParams } from "react-router-dom";
-export const ViewSpecificReport = () => {
+import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useNavigate, useParams, Link } from 'react-router-dom';
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+} from '@mui/material';
+
+export const SpecificReportView = () => {
   const [loading, setLoading] = useState(true);
   const [reportData, setReportData] = useState([]);
   const [userData, setUserData] = useState({});
   const [error, setError] = useState(null);
   const { userId } = useParams();
+
   useEffect(() => {
-    const accesstoken = sessionStorage.getItem("access_token");
+    const accessToken = sessionStorage.getItem('access_token');
+
     async function fetchReportData() {
       try {
         const response = await fetch(
-          "https://timesheet-api-main.onrender.com/view/reports/64de52055b94379e6d9d9f7b",
+          `https://timesheet-api-main.onrender.com/view/reports/${userId}`,
           {
             headers: {
-              "x-api-key": "a57cca53d2086ab3488b358eebbca2e7",
+              'x-api-key': 'a57cca53d2086ab3488b358eebbca2e7',
               Authorization: `Bearer ${accessToken}`,
             },
           }
@@ -28,7 +39,7 @@ export const ViewSpecificReport = () => {
           setUserData(responseData.data.user);
           setReportData(responseData.data.report);
         } else {
-          alert("Error occured while fetching user's report");
+          alert("Error occurred while fetching user's report");
         }
       } catch (error) {
         setError(error.message);
@@ -36,18 +47,23 @@ export const ViewSpecificReport = () => {
         setLoading(false);
       }
     }
+
     fetchReportData();
   }, [userId]);
+
   const navigate = useNavigate();
+
   const handleLogout = () => {
-    sessionStorage.removeItem("access_token");
-    sessionStorage.removeItem("user_id");
-    navigate("/");
+    sessionStorage.removeItem('access_token');
+    sessionStorage.removeItem('user_id');
+    navigate('/');
   };
+
   return (
     <div>
       <Helmet>
         <title> VIEW SPECIFIC REPORTS </title>
+        <link rel="icon" type="image/png" href="./assets/Images/adviewicon.png" />
       </Helmet>
       {loading ? (
         <div className="text-center mt-11">
@@ -57,77 +73,104 @@ export const ViewSpecificReport = () => {
         <div className="text-center mt-4 text-red-500">{error}</div>
       ) : (
         <div>
+          <div className="flex w-full h-[75px] justify-end items-center bg-[#232f3e]">
+            <Link to="/view-users">
+              <button className="bg-gray-500 text-white rounded-md p-1 m-1">
+                GO BACK
+              </button>
+            </Link>
+            <button
+              className="bg-gray-500 text-white rounded-md p-1 mr-[3%]"
+              onClick={handleLogout}
+            >
+              {' '}
+              LOGOUT{' '}
+            </button>
+          </div>
           <div className="text-center mt-4">
             <h2 className="font-extrabold text-2xl font-serif">User Details</h2>
             <p>
-              <span className="font-semibold">Email:</span> {userData.email}
+              <span className="font-semibold">Email:</span> {userData.email}{' '}
             </p>
             <p>
-              <span className="font-semibold">Username:</span>{" "}
-              {userData.username}
+              <span className="font-semibold">Username:</span> {userData.username}
             </p>
           </div>
           <div className="sm:overflow-x-scroll lg:overflow-x-hidden">
-            <table className=" mx-auto">
-              <thead>
-                <th className="border-2 border-solid border-black p-1">DATE</th>
-                <th className="border-2 border-solid border-black p-1">
+            <Paper>
+            <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell className="border-2 border-solid border-black p-1">
+                  S/N
+                </TableCell>
+                <TableCell className="border-2 border-solid border-black p-1">
+                  DATE
+                </TableCell>
+                <TableCell className="border-2 border-solid border-black p-1">
                   PROJECT
-                </th>
-                <th className="border-2 border-solid border-black p-1">TASK</th>
-                <th className="border-2 border-solid border-black p-1">
+                </TableCell>
+                <TableCell className="border-2 border-solid border-black p-1">
+                  TASK
+                </TableCell>
+                <TableCell className="border-2 border-solid border-black p-1">
                   STATUS
-                </th>
-                <th className="border-2 border-solid border-black p-1">
+                </TableCell>
+                <TableCell className="border-2 border-solid border-black p-1">
                   DURATION
-                </th>
-                <th className="border-2 border-solid border-black p-1">LINK</th>
-              </thead>
-              <tbody>
-                {Object.keys(reportData).map((dayOfWeek, index) => {
-                  const report = reportData[dayOfWeek];
-                  if (!report) {
-                    return null;
-                  }
-                  return (
-                    <tr key={index}>
-                      <td className="border-2 border-solid border-black p-3 ">
-                        {index + 1}
-                      </td>
-                      <td className="border-2 border-solid border-black p-3 ">
-                        {" "}
-                        {new Date(report.date).toDateString()}
-                      </td>
-                      <td className="border-2 border-solid border-black p-3 ">
-                        {report.project}
-                      </td>
-                      <td className="border-2 border-solid border-black p-3 ">
-                        {report.task}
-                      </td>
-                      <td className="border-2 border-solid border-black p-3 ">
-                        {report.status}
-                      </td>
-                      <td className="border-2 border-solid border-black p-3 ">
-                        {report.duration}
-                      </td>
-                      <td className="border-2 border-solid border-black p-3 text-blue-500">
-                        <a
-                          href={
-                            report.link.startsWith("http")
-                              ? report.link
-                              : `http://${report.link}`
-                          }
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          {report.link}
-                        </a>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                </TableCell>
+                <TableCell className="border-2 border-solid border-black p-1">
+                  LINK
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Array.isArray(reportData) && reportData.length === 0 ? (
+                <TableRow>
+                  <TableCell colSpan={7} align="center">
+                    No data available
+                  </TableCell>
+                </TableRow>
+              ) : (
+                Object.entries(reportData).map(([dayOfWeek, report], index) => (
+                  <TableRow key={index}>
+                    <TableCell className="border-2 border-solid border-black p-3">
+                      {index + 1}
+                    </TableCell>
+                    <TableCell className="border-2 border-solid border-black p-3">
+                      {new Date(report.date).toDateString()}
+                    </TableCell>
+                    <TableCell className="border-2 border-solid border-black p-3">
+                      {report.project}
+                    </TableCell>
+                    <TableCell className="border-2 border-solid border-black p-3">
+                      {report.task}
+                    </TableCell>
+                    <TableCell className="border-2 border-solid border-black p-3">
+                      {report.status}
+                    </TableCell>
+                    <TableCell className="border-2 border-solid border-black p-3">
+                      {report.duration}
+                    </TableCell>
+                    <TableCell className="border-2 border-solid border-black p-3 text-blue-500">
+                      <a
+                        href={
+                          report.link.startsWith('http')
+                            ? report.link
+                            : `http://${report.link}`
+                        }
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {report.link}
+                      </a>
+                    </TableCell>
+                  </TableRow>
+                ))
+              )}
+            </TableBody>
+          </Table>
+            </Paper>
           </div>
         </div>
       )}
